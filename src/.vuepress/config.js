8785116b14ef80b486e0a6856c9544edf8bd2922
Yue <79188080+Yue-cn@users.noreply.github.com>
2023-01-18 12:57:08 +0800

    Doc

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -3,6 +3,7 @@ module.exports = {
     description: '方便您的使用',
 
     themeConfig: {
+        sidebar: 'true',
         nav:[
             { text: '首页', link: '/' },
             { text: '常见问题', link: '/problems/' },
