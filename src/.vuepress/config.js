21a40980db6017a5ce071629af58b787a4cc6eed
Yue <79188080+Yue-cn@users.noreply.github.com>
2023-01-18 11:56:43 +0800

    App

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,11 +1,12 @@
 module.exports = {
     title: 'OpenFrp Docs',
     description: '方便您的使用',
-    nav:[
-        { text: '首页', link: '/' },
-        { text: '常见问题', link: '/problems/' },
-    ],
+
     themeConfig: {
+        nav:[
+            { text: '首页', link: '/' },
+            { text: '常见问题', link: '/problems/' },
+        ],
         lastUpdated: '该文章最后更新于: ', // string | boolean
     }
 }
