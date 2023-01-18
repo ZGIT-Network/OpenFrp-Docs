5c63dc3f12997edfb0bf54ce57d185a832c16ffe
Yue <79188080+Yue-cn@users.noreply.github.com>
2023-01-18 11:55:12 +0800

    Update Nav

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,7 +1,10 @@
 module.exports = {
     title: 'OpenFrp Docs',
     description: '方便您的使用',
-
+    nav:[
+        { text: '首页', link: '/' },
+        { text: '常见问题', link: '/problems/' },
+    ],
     themeConfig: {
         lastUpdated: '该文章最后更新于: ', // string | boolean
     }
