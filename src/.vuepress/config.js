5509dd2d856cb72689cbf6e7019aa622c8d71530
Yue <79188080+Yue-cn@users.noreply.github.com>
2023-01-18 14:08:41 +0800

    v2ary

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -7,6 +7,7 @@ module.exports = {
         nav:[
             { text: '首页', link: '/' },
             { text: '常见问题', link: '/problems/' },
+            { text: '使用教程', link: '/use/' }
         ],
         lastUpdated: '该文章最后更新于: ', // string | boolean
     }
