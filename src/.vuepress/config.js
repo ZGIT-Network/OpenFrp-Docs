55e26e7693b3dc4005ac02ba056685ad5201baea
skyxingcheng233 <skyxingcheng233@outlook.com>
2024-05-22 10:23:56 +0800

    自启动指南

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -3,17 +3,18 @@ module.exports = {
     description: '分享每一份技术。',
     plugins: [
     [
-      '@vuepress/last-updated',
-      {
-        transformer: (timestamp, lang) => {
-          // 不要忘了安装 moment
-          const moment = require('moment')
-          moment.locale(lang)
-          return moment(timestamp).fromNow()
-        }
-      }
+      // 不知道这是干嘛的, 但是它部署失败
+      // '@vuepress/last-updated',
+      // {
+      //   transformer: (timestamp, lang) => {
+      //     // 不要忘了安装 moment
+      //     const moment = require('moment')
+      //     moment.locale(lang)
+      //     return moment(timestamp).fromNow()
+      //   }
+      // }
     ]
-  ]
+  ],
     themeConfig: {
         sidebar: {
             '/use/':[
@@ -26,7 +27,8 @@ module.exports = {
                         ['','基本教程'],
                         ['desktop-launcher','桌面启动器'],
                         ['frpc','FRPC 使用教程'],
-                        ['proxy-protocol','反馈用户真实IP'] 
+                        ['boot-on-startup','FRPC 自启动指南'],
+                        ['proxy-protocol','反馈用户真实IP']
                     ]
                 },
                 {
@@ -48,14 +50,14 @@ module.exports = {
         docsBranch: 'main',
         editLinks:true,
         editLinkText: '帮我们编辑该页面!',
-        
+
         nav:[
             { text: '首页', link: '/' },
             { text: '常见问题', link: '/problems/' },
             { text: '使用教程', link: '/use/' },
             { text: '配置指南', link: '/guides/' }
         ],
-            
+
         lastUpdated: '该文章最后更新于 ', // string | boolean
     }
 }
